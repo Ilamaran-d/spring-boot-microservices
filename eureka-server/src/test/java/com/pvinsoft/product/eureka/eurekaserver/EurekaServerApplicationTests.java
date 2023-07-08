b844bcf16a96f20c2d9c93ab91b2e9d2c93edbd4
Ila <Ilamaran>
2023-07-08 17:08:24 +0800

    Initial push for eureka server

---
+++ eureka-server/src/test/java/com/pvinsoft/product/eureka/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pvinsoft.product.eureka.eurekaserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
